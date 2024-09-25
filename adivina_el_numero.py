9e505fcc66919850b37ff6e45e55ee8b9ff15127
DylanAaronElicserioMartinez <a.23308051281179@cbtis128.edu.mx>
2024-09-24 20:09:27 -0600

    Add files via upload

---
+++ adivina_el_numero.py
@@ -0,0 +1,15 @@
+print(" ")
+print("Dylan Aaron Elicserio Martínez 3°W ")
+print(" ")
+#numero a adivinar
+numero_a_adivina = 10
+
+#pedir al usuario que adivine
+intento = int(input("adivine el numero del 1 al 100:"))
+
+#verificar si el intento es correcto
+if intento == numero_a_adivina:
+ print("Correcto")
+else:
+ print("Incorrecto")
+ 
